b841cf6c28154adde99ee4ca4a1bdb22742f578b
Laurel Schwulst <laurel.schwulst@gmail.com>
2016-03-24 02:46:21 -0400

    statcounter

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -5,5 +5,16 @@
 	  	
   ?>
 
+	<script type="text/javascript">
+		var sc_project=10134633; 
+		var sc_invisible=1; 
+		var sc_security="f66685a1"; 
+		var scJsHost = (("https:" == document.location.protocol) ?
+		"https://secure." : "http://www.");
+		document.write("<sc"+"ript type='text/javascript' src='" +
+		scJsHost+
+		"statcounter.com/counter/counter.js'></"+"script>");
+	</script>
+
 </body>
 </html>
