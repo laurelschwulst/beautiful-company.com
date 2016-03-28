be906ea063be91c0c10697e08c9f82314dbf5de3
Laurel Schwulst <laurel.schwulst@gmail.com>
2016-03-28 15:44:47 -0400

    update

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -22,5 +22,20 @@
 
   ?>
 
-</head>
+  <script type="text/javascript">
+    var _paq = _paq || [];
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+      var u="//beautiful-company.com/analytics/";
+      _paq.push(['setTrackerUrl', u+'piwik.php']);
+      _paq.push(['setSiteId', 1]);
+      var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+      g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+    })();
+  </script>
+  <noscript><p><img src="//beautiful-company.com/analytics/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+
+  </head>
+
 <body>
